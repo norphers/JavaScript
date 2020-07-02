8af4ffe758cac4f01cfe7a0ac40f59b10f9e9d93
ocanades <ocanades@gmail.com>
2020-07-02 13:35:50 +0200

    exercises 1 and 2 - variables and first part of type conversion

---
+++ Section2/typeConversion.js
@@ -0,0 +1,9 @@
+// JavaScript Type Conversion
+
+var lastName, phoneNumber, gender;
+lastName="Doe";
+phoneNumber=666885522;
+gender='man';
+
+console.log("He is a " + gender + ", his last name is " + lastName + 
+            ", and his phone number is " + phoneNumber +".");
