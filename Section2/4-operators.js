ff18e2817467d9afa4b547b9851b017fbc0d5f51
ocanades <ocanades@gmail.com>
2020-07-03 21:17:11 +0200

    Section2

---
+++ Section2/4-operators.js
@@ -0,0 +1,29 @@
+// Mathematic operators: +, -, *, /
+// Logical operators: <, >, <=, =>
+// * typeof
+// arithmetic unary operators: ++, --, +=, -=
+// operadores de asignación: ==, 
+
+var age1, age2;
+
+age1=34;
+age2=28;
+
+var ageDifference = age1 - age2;
+console.log(ageDifference);
+
+var ageAddition = age1 + age2;
+console.log(ageAddition);
+
+var ageMultiplication = age1 * age2;
+console.log(ageMultiplication);
+
+var ageDivide = age1 / age2;
+console.log(ageDivide);
+
+var a = 5;
+var b = 10;
+
+// a = a+b == a+=b
+// c = a % b; == a %= b
+
--- Section2/typeConversion.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Type Conversion
-
-var lastName, phoneNumber, gender;
-lastName="Doe";
-phoneNumber=666885522;
-gender='man';
-
-console.log("He is a " + gender + ", his last name is " + lastName + 
-            ", and his phone number is " + phoneNumber +".");
